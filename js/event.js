a95d3f5b628cd81c68d305eb2c0430e5bad02f7a
Travis <travis@devternity.com>
2022-03-09 10:14:41 +0000

    Updates

--- js/event.js
+++ js/event.js
@@ -289,7 +289,7 @@
             "country": "us",
             "bio": "Principal Community Architect for Web Platform @ Microsoft, Blogger, Podcaster",
             "img": "images/hanselman.png", 
-            "title": "Skills for a Successful Programmer",
+            "title": "Unusual Skills for a Successful Programmer",
             "description": "In this session, Scott Hanselman will share his career journey from a programmer to a Lead .NET Program Manager at Microsoft. Expect some (unusual) advice on skills and practices you need to succeed.",
             "tags": ["career advice", "growth", "skills"]
           },
