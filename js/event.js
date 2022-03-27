3cef3414c95b8cc2801c89361928141644aa1606
Travis <travis@devternity.com>
2022-03-27 13:49:16 +0000

    Updates

--- js/event.js
+++ js/event.js
@@ -10,7 +10,7 @@
     "talks_total": 20,
     "workshops_total": 7,
     "city": "Online",
-    "venue_name": "Eastern European Time",
+    "venue_name": "GMT",
     "contactEmail": "hello@devternity.com",
     "status": "on_sale",
     "country": "Latvia",
